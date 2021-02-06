3dd7542ffca8bd86d66122c507fbfa29111a1673
Philip Gonzalez <pgonzalez115@gmail.com>
2021-02-06 13:09:33 -0700

    Update blink file

--- src/blink.s
+++ src/blink.s
@@ -1,2 +1,15 @@
+    .org $8000      ; For our processeor, the ROM address space starts at $8000
     lda #$ff
     sta $6002
+
+    lda #$55
+    sta $6000
+
+    lda #$aa
+    sta $6000
+
+    jmp $8005
+    
+    .org $fffc      ; Go to $fffc, set the init vector and pad the last two bytes.
+    .word $8000
+    .word $0000
